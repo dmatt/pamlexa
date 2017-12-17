87da4b0c9c29d06b73c759d2655741650e11e6c4
Glitch (pamlexa) <none>
2017-12-17 03:21:07 +0000

    🐃🏄 Add a Disqus replies intent and use fixtures in place of API data

--- index.js
+++ index.js
@@ -26,36 +26,38 @@ alexaApp.launch(function(request, response) {
 
 // The main Disqus intent - checks if a username was supplied or not and sends the appropriate response
 alexaApp.intent("Replies", {
-    "slots": { "WHEN": "AMAZON.DATE" },
+    "slots": { "USERNAME": "LIST_OF_USERNAMES" },
     "utterances": [
-      "read me my replies",
+      "test with {USERNAME}",
+      "read the replies",
+      "read my replies",
+      "read my recent replies",
+      "tell me my replies",
+      "tell me my recent replies",
+      "read the replies to {USERNAME}",
       "what are my recent replies",
+      "do I have any replies",
       "do I have any recent replies",
-      "do I have any recent replies",      
-      "what's the weather for {WHEN}",
-      "what I should expect on {WHEN}",
-      "tell me the weather",
-      "{WHEN}"
     ]
   },
   function(request, response) {
-    console.log("In Weather intent");
+    console.log("In Replies intent");
     // If the requester specified a date/day
-    if (request.data.request.intent.slots.WHEN &&
-        request.data.request.intent.slots.WHEN.value) {
-        // Request the weather - we're using Promises to delay the response until we have data back from forecast.io
-        return getWeather(new Date(request.data.request.intent.slots.WHEN.value))
-          .then(function (weather) {
-            console.log('Responding to weather request for ' + request.data.request.intent.slots.WHEN.value + ' with:', weather);
-            response.say(weather);
+    if (true /* request.data.request.intent.slots.USERNAME &&
+        request.data.request.intent.slots.USERNAME.value */) {
+        // Request the replies from Disqus - we're using Promises to delay the response until we have data back from Disqus API
+        return getReplies(request.data.request.intent.slots.USERNAME.value)
+          .then(function (replies) {
+            console.log('Responding to replies request for ' + request.data.request.intent.slots.USERNAME.value + ' with:', replies);
+            response.say(replies);
           })
           .catch(function(err){
             response.say(err);
           });
     } else {
       // If the requester didn't specify a date/day
-      console.log('Responding to weather request with no day/date');
-      response.say('I can tell you the weather<break time="1s"/> but you must give me a day');
+      console.log('Responding to replies request with no username');
+      response.say('I can give you some comment replies<break time="500ms"/> but I need to know your Discuss username!');
     }
   }
 );
@@ -154,6 +156,48 @@ function getWeather(day) {
   });
 }
 
+// Looks up recent replies for the username provided, using Disqus API
+function getReplies(username) {
+  return new Promise(function(resolve, reject) {
+    //if (!username /*|| username.toString() === 'Invalid Date'*/) {
+      //return reject('Invalid usernames for Discuss replies!');
+    //} 
+    
+    // fixture
+    username = "cheese"
+
+    request({
+      url: BASE_URL,
+      json: true
+    }, function(err, res, body) {
+      var data, text, card,
+          simpleUsername = username /* day.toISOString().split('T')[0];*/
+
+      if (err || res.statusCode >= 400) {
+        console.error(res.statusCode, err);
+        return reject('Unable to get Disqus data!');
+      }
+
+      /*body.daily.data.forEach(function(dailyData) {
+        if ((new Date(dailyData.time * 1000)).toISOString().split('T')[0] === simpleUsername) {
+          data = dailyData;
+        }
+      });*/
+      
+      // fixture
+      data = simpleUsername;
+
+      if (!data) {
+       return reject('I have no data for that username!');
+      }
+
+      text = getRepliesText(data);
+
+      resolve(text);
+    });
+  });
+}
+
 // Converts the weather data into an understandable text string
 function getWeatherText(data) {
   var conditions;
@@ -193,6 +237,52 @@ function getWeatherText(data) {
   }
 
   return conditions;
+}
+
+// Converts the Disqus replies data into an understandable text string
+function getRepliesText(data) {
+  // set a variale that we'll change based on what is inside data, for example if contains special characters strip, if an image say so.
+  var reply, conditions;
+  
+  reply = "Hello there, " + data;
+  /*
+  if (data.precipProbability > 0.7 && data.precipIntensityMax > 0.05) {
+    if (data.precipType === 'rain') {
+      conditions = 'Don\'t forget your umbrella.';
+    } else {
+      conditions = 'Brace yourself for the snow.';
+    }
+  } else if (data.temperatureMax > 93 || data.apparentTemperatureMax > 98) {
+    if (data.dewPoint > 72 || data.humidity > 0.75) {
+      conditions = 'It\'s going to be nasty.';
+  } else {
+      conditions = 'Prepare for a scorcher.';
+    }
+  } else if (data.temperatureMax < 35) {
+    if (data.windSpeed > 15) {
+      conditions = 'Prepare for bitter cold wind in your face.';
+    } else {
+      conditions = 'Bitterly cold temperatures are in store for.';
+    }
+  } else if (data.dewPoint > 72 && data.humidity > 0.75) {
+    conditions = 'The humidity is going to be brutal.';
+  } else if (data.cloudCover > 0.85) {
+    conditions = 'It will be very cloudy.';
+  } else if (data.cloudCover < 0.1) {
+    if (data.windSpeed > 15) {
+      conditions = 'Lots of sun and breezy conditions are in store.';
+    } else {
+      conditions = 'There will be lots of sunshine.';
+    }
+  } else if (data.windSpeed > 20) {
+    conditions = 'It\'s going to be gusty.';
+  } else {
+    conditions = data.summary;
+  }
+  
+  */
+
+  return reply;
 }
 
 app.listen(PORT, () => console.log("Listening on port " + PORT + "."));
